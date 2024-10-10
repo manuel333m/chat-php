3c9b28514dbb70808fb342cedd9b66ce7e23df0f
manuel333m <manumendozao.602@gmail.com>
2024-10-10 10:17:07 -0400

    chat 4

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
-$dbDatabase = 'chat_edison';
+$dbDatabase = 'tutorial_db';
 $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
